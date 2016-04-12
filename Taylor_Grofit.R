c04eade82538e65b6f4de1ce92651c0f8969eed2
Taylor Dunivin <dunivita@Taylors-MacBook-Pro.local>
2016-04-12 09:08:45 -0400

    includes parameter graphs

--- Taylor_Grofit.R
+++ Taylor_Grofit.R
@@ -1,6 +1,7 @@
 library(grofit)
 library(reshape2)
 library(dplyr)
+library(ggplot2)
 
 #read in raw data
 data=read.table("082315_MIC.asc")
@@ -21,7 +22,7 @@ m=ncol(data)
 time=matrix(rep(time, m), c(n, m))
 time=t(time)
 
-#transverse the data
+#transpose the data
 data=t(data)
 
 #read in platemap
@@ -29,7 +30,137 @@ platemap=read.csv("20150823_platemap.csv")
 
 #combine data with platemap and remove well column
 data=cbind(platemap, data)
+
+#Well's were labeled twice, so redundant naming was removed
 data=data[,-1]
 
-#run grofit
-grofit(time, data, TRUE)
+#OPTION: run all of grofit
+results=grofit(time, data)
+
+#OPTION: only run table 
+results=gcFit(time, data)
+
+#make a data frame of the results
+results=data.frame(read.table(results), header=TRUE)
+
+#extract maximum growth parameter from dataset
+mu=results$mu.spline
+
+#add information to mu
+mu=cbind(platemap, mu)
+
+#add replicate number to data
+reps=data.frame(read.table("Replicates.txt", header=TRUE))
+mu=cbind(mu, reps)
+
+#remove control values with replicate numbers
+normalization=subset(mu, Concentration %in% 0)
+
+#remove unimportant columns
+normalization=normalization[,-1]
+normalization=normalization[,-2]
+normalization=normalization[,-2]
+
+#add negatives back
+mu=inner_join(mu, normalization, by = c("Strain", "Replicate"))
+
+#normalize data
+norm=mu$mu.x/mu$mu.y
+norm=data.frame(norm)
+#add information to normalized data
+mu=cbind(mu, norm)
+
+#group data
+grouped=group_by(mu, Strain, Concentration)
+
+#Calculate average and stdev
+stats=summarise(grouped, Average=mean(norm), StDev=sd(norm))
+
+#plot data
+ggplot(data=stats, aes(x=Concentration, y=Average)) +
+  geom_line(size=1.5) +
+  geom_point(aes(color=Concentration), size=2.5) +
+  geom_point(shape=1, size=2.5, color="black") +
+  scale_color_gradientn(colors=rainbow(6)) +
+  facet_wrap(~Strain)
+
+#Get OD590 maximum data (A)
+A=results$A.spline
+A=cbind(platemap, A)
+
+#add replicate number to data
+reps=data.frame(read.table("Replicates.txt", header=TRUE))
+A=cbind(A, reps)
+
+#remove control values with replicate numbers
+normalization=subset(A, Concentration %in% 0)
+
+#remove unimportant columns
+normalization=normalization[,-1]
+normalization=normalization[,-2]
+normalization=normalization[,-2]
+
+#add negatives back
+A=inner_join(A, normalization, by = c("Strain", "Replicate"))
+
+#normalize data
+norm=A$A.x/A$A.y
+
+#add information to normalized data
+A=cbind(A, norm)
+
+#group data
+grouped=group_by(A, Strain, Concentration)
+
+#Calculate average and stdev
+stats=summarise(grouped, Average=mean(norm), StDev=sd(norm))
+
+#plot data
+ggplot(data=stats, aes(x=Concentration, y=Average)) +
+  geom_line(size=1.5) +
+  geom_point(aes(color=Concentration), size=2.5) +
+  geom_point(shape=1, size=2.5, color="black") +
+  scale_color_gradientn(colors=rainbow(6)) +
+  facet_wrap(~Strain)
+
+#extract time to exponential growth parameter (lambda) from dataset
+lambda=results$lambda.spline
+
+#add information to mu
+lambda=cbind(platemap, lambda)
+
+#add replicate number to data
+reps=data.frame(read.table("Replicates.txt", header=TRUE))
+lambda=cbind(lambda, reps)
+
+#remove control values with replicate numbers
+normalization=subset(lambda, Concentration %in% 0)
+
+#remove unimportant columns
+normalization=normalization[,-1]
+normalization=normalization[,-2]
+normalization=normalization[,-2]
+
+#add negatives back
+lambda=inner_join(lambda, normalization, by = c("Strain", "Replicate"))
+
+#normalize data
+norm=lambda$lambda.x/lambda$lambda.y
+
+#add information to normalized data
+lambda=cbind(lambda, norm)
+
+#group data
+grouped=group_by(lambda, Strain, Concentration)
+
+#Calculate average and stdev
+stats=summarise(grouped, Average=mean(norm), StDev=sd(norm))
+
+#plot data
+ggplot(data=stats, aes(x=Concentration, y=Average)) +
+  geom_line(size=1.5) +
+  geom_point(aes(color=Concentration), size=2.5) +
+  geom_point(shape=1, size=2.5, color="black") +
+  scale_color_gradientn(colors=rainbow(6)) +
+  facet_wrap(~Strain)
+
